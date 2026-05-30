30828de0a63a2efa15648a2a6619636485519238
kunalmaratha <kunal6264866372@gmail.com>
2026-05-30 21:57:53 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def footer_home():
+   
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by  kunal maratha</p>  
+       
+        </div>
+                
+                """, unsafe_allow_html=True)
